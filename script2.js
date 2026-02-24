eab7dac6e7731544d81d6d030825352d7238dd47
hirdyanshg <hirdyanshgoyal7@gmail.com>
2026-02-24 10:32:46 +0530

    3rd commit

---
+++ script2.js
@@ -0,0 +1,29 @@
+//operators
+{const a=10;
+const b=20;
+console.log(a+b);
+}
+{
+const a=10;
+const b=20;
+console.log(a-b);
+}
+{
+const a=10;
+const b=20;
+console.log(a*b);
+}
+{
+const a=10;
+const b=20;
+console.log(a/b);
+}
+{
+    var count;
+            document.write("Starting Loop" + "<br/>");
+            for(count=0; count<10; count++){
+                document.write("Current Count: " + count );
+                document .write("<br/>");
+            }
+                document.write("Loop Stopped ");
+}
